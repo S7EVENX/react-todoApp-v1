cb56c68df3b16678a20442e182b8723f164611da
S7EVENX <97410026+S7EVENX@users.noreply.github.com>
2022-11-03 17:24:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-todoApp-v1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bwqbt8)
---
+++ src/components/TodoApp.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function TodoApp() {
+  return (
+    <div className="todoContainer">
+
+      <h1> Hola </h1>
+      
+    </div>
+  );
+}
+
+export default TodoApp;
